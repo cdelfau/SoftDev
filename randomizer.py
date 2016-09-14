d3d5e798036a761df53c4045ac8c84e44425fb36
cdelfau <cdelfau@stuy.edu>
2016-09-13 22:47:23 -0400

    added python function to pick a random person from a class given the period of that class and ran 2 test runs per period

---
+++ randomizer.py
@@ -0,0 +1,18 @@
+import random
+
+p4 = ['Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson','Brian', 'Farhan', 'Janet', 'Harry', 'Kevin', 'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth', 'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj', 'Amy', 'Arvind', 'Nobel', 'Angela', 'Edward', 'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina']
+
+p8 = ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki', 'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda', 'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vncent', 'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel', 'Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio', 'Kelly', 'William', 'Jordan', 'Haley', 'Henry']
+
+p9 =  ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C', 'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha', 'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry', 'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine', 'Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L', 'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
+
+def randomStudent(period):
+    print(random.choice(period))
+    return;
+
+randomStudent(p4)
+randomStudent(p4)
+randomStudent(p8)
+randomStudent(p8)
+randomStudent(p9)
+randomStudent(p9)
